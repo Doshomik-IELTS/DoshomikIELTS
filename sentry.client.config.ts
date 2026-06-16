13ab5da7a21fa532dcee7ecf0c851cea2c529c3f
Ann Naser Nabil <ann.n.nabil@gmail.com>
2026-06-17 03:48:15 +0600

    Add sentry.client.config.ts for Sentry v10 client init

---
+++ sentry.client.config.ts
@@ -0,0 +1,16 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+  sendDefaultPii: false,
+
+  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
+
+  integrations: [],
+
+  ignoreErrors: [
+    "NEXT_REDIRECT",
+    "NEXT_NOT_FOUND",
+  ],
+});
